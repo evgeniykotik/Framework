d63a2457d34e221773b64fd6e6025539ac2fa73b
evgeniykotik <postoyalkoevgeniy@gmail.com>
2022-12-07 12:26:53 +0300

    task 3

---
+++ Fw/Core/Component/Base.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fw\Core\Component;
+
+abstract class Base
+{
+    public array $result;
+    public string $id;
+    public array $params;
+
+    public function __construct()
+    {
+
+    }
+
+    public function executeComponent()
+    {
+
+    }
+}
